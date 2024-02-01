6564c8a47ee47965af59721ee2557645b98d0091
Kacper Jarocki <kacper.jarocki1121@gmail.com>
2024-02-01 19:40:04 +0100

    init

---
+++ coders-place-backend/README.md
@@ -0,0 +1,2 @@
+# coders-place-backend
+Web app backend for final engineering project, social service for coders
---
+++ coders-place-backend/src/test/java/com/service/coders/CodersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.coders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
