9c178ba320804f096b9bf83181ffe5bf94b48a80
KacperJarocki <dev.kacperjarocki@proton.me>
2024-09-10 11:26:17 +0200

    changed name

--- coders-space-frontend/README.md
+++ coders-space-frontend/README.md
@@ -1,6 +1,6 @@
-# Coders
+# CodersSpaceFrontend
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.4.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.9.
 
 ## Development server
 
--- coders-space-frontend/src/app/app.component.spec.ts
+++ coders-space-frontend/src/app/app.component.spec.ts
@@ -2,9 +2,11 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -12,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'coders'`, () => {
+  it(`should have the 'coders-space-frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('coders');
+    expect(app.title).toEqual('coders-space-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('coders app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, coders-space-frontend');
   });
 });
