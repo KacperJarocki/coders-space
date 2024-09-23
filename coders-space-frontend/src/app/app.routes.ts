d0293eb96c5c4f1504887b5e8454b9e5f64c2bd9
KacperJarocki <dev.kacperjarocki@proton.me>
2024-09-23 19:55:20 +0200

    Created basic forms
    Created only form and mapped it to variables,
    Have to send it to server(have todo reasearch),
    it looks ugly will change it later to popup,

--- coders-space-frontend/src/app/app.routes.ts
+++ coders-space-frontend/src/app/app.routes.ts
@@ -1,17 +1,21 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
 export const routes: Routes = [
   {
     path: "",
     component: HomeComponent,
-    title: "CoderSpace"
+    title: "home"
+  },
+  {
+    path: "sign-up",
+    component: SignUpComponent,
+    title: "sign-up"
   },
   {
     path: "login",
-    component: SignUpComponent,
-    title: "Login"
+    component: LoginComponent,
+    title: "login"
   }
-
-
 ];
