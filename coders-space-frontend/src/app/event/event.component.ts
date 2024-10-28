815d00bc2933c3ebc95a3eb709aa557c2df3ef81
KacperJarocki <dev.kacperjarocki@proton.me>
2024-10-28 13:34:00 +0100

    Deleting event works

--- coders-space-frontend/src/app/event/event.component.ts
+++ coders-space-frontend/src/app/event/event.component.ts
@@ -1,14 +1,35 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Event } from '../interfaces/event';
-
+import { CommonModule } from '@angular/common';
+import { EventService } from '../services/event.service';
 @Component({
   selector: 'app-event',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './event.component.html',
   styleUrl: './event.component.css'
 })
 export class EventComponent {
+  constructor(private eventService: EventService) { }
   @Input() event!: Event;
-
+  @Output() refreshList: EventEmitter<void> = new EventEmitter<void>();
+  emitRefresh(): void {
+    this.refreshList.emit();
+  }
+  editEvent(): void {
+    console.log('Editing event');
+    this.emitRefresh();
+  }
+  removeEvent(): void {
+    console.log("deleting event");
+    this.eventService.deleteEvent(this.event).subscribe({
+      next: (response: any) => {
+        console.log('Event deleted', response);
+        this.emitRefresh();
+      },
+      error: (error: any) => {
+        console.error('There was an error!', error);
+      },
+    });
+  }
 }
