90b0ff119298d95269e0b663f85a22f94f57430d
Kacper Jarocki <dev.kacperjarocki@proton.me>
2024-11-09 17:38:49 +0100

    adding deleting and editing tag works
    has to do autorefreshing

--- coders-space-frontend/src/app/tags/tag-form/tag-form.component.ts
+++ coders-space-frontend/src/app/tags/tag-form/tag-form.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Tag } from '../../interfaces/tag';
+import { CommonModule } from '@angular/common';
+import { TagService } from '../../services/tag.service';
+import { TagsPublicationsEvents } from '../../interfaces/tags-publications-events';
+import { Publication } from '../../interfaces/publication';
+import { Event } from '../../interfaces/event';
 
 @Component({
   selector: 'app-tag-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './tag-form.component.html',
   styleUrl: './tag-form.component.css'
 })
 export class TagFormComponent {
+  @Input() tag?: TagsPublicationsEvents;
+  @Input() publication?: Publication;
+  @Input() event?: Event;
+  tagForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private tagService: TagService) {
+    this.tagForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(1)]]
+    });
+  }
+
+  ngOnInit(): void {
+    if (this.tag) {
+      this.tagForm.patchValue(this.tag.tag);
+    }
+  }
+
+  onSubmit(): void {
+    if (this.tagForm.valid) {
+      const tagData: Tag = {
+        id: this.tag?.tag.id ?? 0,
+        name: this.tagForm.value.name
+      }
+      const tagToSave: TagsPublicationsEvents = {
+        id: this.tag?.id ?? 0,
+        tag: tagData,
+        publicationId: this.publication?.id ?? this.tag?.publicationId ?? null,
+        eventId: this.event?.id ?? this.tag?.eventId ?? null
+      }
+      console.log(tagToSave);
+      this.tagService.createTag(tagToSave).subscribe({
+        next: () => console.log('Tag saved' + tagToSave.publicationId + "  " + tagToSave.eventId + tagToSave.tag.name),
+        error: (err) => console.error('Failed to save tag:', err)
+      });
+    }
+  }
 }
--- coders-space-frontend/src/app/tags/tag-list/tag-list.component.ts
+++ coders-space-frontend/src/app/tags/tag-list/tag-list.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Publication } from '../../interfaces/publication';
+import { Event } from '../../interfaces/event';
+import { ComponentListState } from '../../types/list-state.type';
+import { TagsPublicationsEvents } from '../../interfaces/tags-publications-events';
+import { TagService } from '../../services/tag.service';
+import { Tag } from '../../interfaces/tag';
+import { TagComponent } from '../tag/tag.component';
+import { TagFormComponent } from '../tag-form/tag-form.component';
 
 @Component({
   selector: 'app-tag-list',
   standalone: true,
-  imports: [],
+  imports: [TagComponent, TagFormComponent],
   templateUrl: './tag-list.component.html',
   styleUrl: './tag-list.component.css'
 })
 export class TagListComponent {
+  @Input() publication?: Publication;
+  @Input() event?: Event;
+  listState: ComponentListState<TagsPublicationsEvents> = { state: 'idle' };
+  constructor(private tagService: TagService) { }
 
+  addTag = false;
+  ngOnInit() {
+    this.refreshTags();
+  }
+  toggleAddTag() {
+    this.addTag = !this.addTag;
+  }
+  refreshTags() {
+    const tag: Tag = { name: "tag", id: 0 };
+    const tagEP: TagsPublicationsEvents = {
+      eventId: this.event?.id ?? null,
+      publicationId: this.publication?.id ?? null,
+      tag: tag,
+      id: 0,
+    };
+    console.log(tagEP);
+    this.listState = { state: 'loading' };
+    this.tagService.retriveTag(tagEP)
+      .subscribe({
+        next: (data) => {
+          this.listState = { state: 'success', results: data };
+        },
+        error: (error) => {
+          this.listState = { state: 'error', error: error };
+        }
+      });
+  }
 }
