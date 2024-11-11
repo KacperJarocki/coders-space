0e4adc2f17e69606edadff07f9f57544d0e73076
Kacper Jarocki <dev.kacperjarocki@proton.me>
2024-11-11 19:52:49 +0100

    Added validatiors for login and signup

--- coders-space-frontend/src/app/login/login.component.ts
+++ coders-space-frontend/src/app/login/login.component.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { FooterComponent } from '../footer/footer.component';
 import { Client } from '../interfaces/client';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -11,39 +12,47 @@ import { Client } from '../interfaces/client';
   imports: [
     RouterLink,
     ReactiveFormsModule,
-    FooterComponent
+    FooterComponent,
+    CommonModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   private url: string = "http://backend.localhost/api/";
-  private http = inject(HttpClient);
-  private router = inject(Router)
-  logInForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
-  });
+  logInForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {
+    this.logInForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
   logIn() {
-    const email = this.logInForm.get('email')?.value;
-    const password = this.logInForm.get('password')?.value;
-    const client: Client = {
-      id: 0,
-      name: '',
-      email: email || '',
-      password: password || '',
-      client_type: ''
-    };
-    this.http.post<{ token: string }>(
-      this.url + "auth/login", client)
-      .subscribe({
+    if (this.logInForm.valid) {
+      const { email, password } = this.logInForm.value;
+      const client: Client = { id: 0, name: '', email, password, client_type: '' };
+
+      this.http.post<{ token: string }>(`${this.url}auth/login`, client).subscribe({
         next: (response) => {
           localStorage.setItem('token', response.token);
-          this.router.navigateByUrl("/")
+          this.router.navigateByUrl("/");
         },
         error: (error) => {
           console.error('Login failed', error);
+          alert('Login failed: Invalid credentials or network error');
         }
       });
+    } else {
+      alert('Please correct the errors in the form.');
+    }
   }
+
+  get email() { return this.logInForm.get('email'); }
+  get password() { return this.logInForm.get('password'); }
 }
