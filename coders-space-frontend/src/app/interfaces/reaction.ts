5f17df2da2336c52b82c3105885f8097f4a18ee0
Kacper Jarocki <dev.kacperjarocki@proton.me>
2024-11-11 11:11:25 +0100

    Interface done

--- coders-space-frontend/src/app/interfaces/reaction.ts
+++ coders-space-frontend/src/app/interfaces/reaction.ts
@@ -1,2 +1,15 @@
 export interface Reaction {
+  id: number;
+  clientId: number;
+  publicationId: number | null;
+  eventId: number | null;
+  commentId: number | null;
+  type: ReactionType;
+}
+enum ReactionType {
+  LIKE,
+  DISLIKE,
+  RTFM,
+  LOVE,
+  LOL,
 }
