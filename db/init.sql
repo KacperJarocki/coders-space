c50f3dedd7d0269d79be330d63a978f47c3dc628
Kacper Jarocki <dev.kacperjarocki@proton.me>
2024-11-14 21:15:32 +0100

    deleting made easier

--- db/init.sql
+++ db/init.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS clients (
     client_type SMALLINT NOT NULL,
     enabled BOOLEAN NOT NULL
 );
+
 CREATE TABLE IF NOT EXISTS publications (
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
@@ -13,8 +14,9 @@ CREATE TABLE IF NOT EXISTS publications (
     client_id INTEGER NOT NULL,
     publication_type SMALLINT NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    FOREIGN KEY (client_id) REFERENCES clients (id)
+    FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE CASCADE
 );
+
 CREATE TABLE IF NOT EXISTS events (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -22,32 +24,35 @@ CREATE TABLE IF NOT EXISTS events (
     date TIMESTAMP NOT NULL,
     created_at TIMESTAMP NOT NULL,
     client_id INTEGER NOT NULL,
-    FOREIGN KEY (client_id) REFERENCES clients (id)
+    FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE CASCADE
 );
+
 CREATE TABLE IF NOT EXISTS tags (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS tags_publications_events(
+CREATE TABLE IF NOT EXISTS tags_publications_events (
     id SERIAL PRIMARY KEY,
     tag_id INTEGER NOT NULL,
     publication_id INTEGER,
     event_id INTEGER,
     FOREIGN KEY (tag_id) REFERENCES tags (id),
-    FOREIGN KEY (publication_id) REFERENCES publications (id),
-    FOREIGN KEY (event_id) REFERENCES events (id)
+    FOREIGN KEY (publication_id) REFERENCES publications (id) ON DELETE CASCADE,
+    FOREIGN KEY (event_id) REFERENCES events (id) ON DELETE CASCADE
 );
+
 CREATE TABLE IF NOT EXISTS comments (
     id SERIAL PRIMARY KEY,
     content TEXT NOT NULL,
     client_id INTEGER NOT NULL,
     publication_id INTEGER,
     event_id INTEGER,
-    FOREIGN KEY (client_id) REFERENCES clients (id),
-    FOREIGN KEY (publication_id) REFERENCES publications (id),
-    FOREIGN KEY (event_id) REFERENCES events (id)
+    FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE CASCADE,
+    FOREIGN KEY (publication_id) REFERENCES publications (id) ON DELETE CASCADE,
+    FOREIGN KEY (event_id) REFERENCES events (id) ON DELETE CASCADE
 );
+
 CREATE TABLE IF NOT EXISTS reactions (
     id SERIAL PRIMARY KEY,
     type SMALLINT NOT NULL,
@@ -55,38 +60,43 @@ CREATE TABLE IF NOT EXISTS reactions (
     publication_id INTEGER,
     event_id INTEGER,
     comment_id INTEGER,
-    FOREIGN KEY (client_id) REFERENCES clients (id),
-    FOREIGN KEY (publication_id) REFERENCES publications (id),
-    FOREIGN KEY (event_id) REFERENCES events (id),
-    FOREIGN KEY (comment_id) REFERENCES comments (id)
+    FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE CASCADE,
+    FOREIGN KEY (publication_id) REFERENCES publications (id) ON DELETE CASCADE,
+    FOREIGN KEY (event_id) REFERENCES events (id) ON DELETE CASCADE,
+    FOREIGN KEY (comment_id) REFERENCES comments (id) ON DELETE CASCADE
 );
+
 CREATE TABLE IF NOT EXISTS reports (
     id SERIAL PRIMARY KEY,
     publication_id INTEGER,
     event_id INTEGER,
     comment_id INTEGER,
     date TIMESTAMP NOT NULL,
-    FOREIGN KEY (publication_id) REFERENCES publications (id),
-    FOREIGN KEY (event_id) REFERENCES events (id),
-    FOREIGN KEY (comment_id) REFERENCES comments (id)
+    FOREIGN KEY (publication_id) REFERENCES publications (id) ON DELETE CASCADE,
+    FOREIGN KEY (event_id) REFERENCES events (id) ON DELETE CASCADE,
+    FOREIGN KEY (comment_id) REFERENCES comments (id) ON DELETE CASCADE
 );
+
 CREATE TABLE IF NOT EXISTS bages (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
+
 CREATE TABLE IF NOT EXISTS clients_bages (
     client_id INTEGER NOT NULL,
     bage_id INTEGER NOT NULL,
-    FOREIGN KEY (client_id) REFERENCES clients (id),
+    FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE CASCADE,
     FOREIGN KEY (bage_id) REFERENCES bages (id)
 );
+
 CREATE TABLE IF NOT EXISTS participations (
     id SERIAL PRIMARY KEY,
     client_id INTEGER NOT NULL,
     event_id INTEGER NOT NULL,
-    FOREIGN KEY (client_id) REFERENCES clients (id),
-    FOREIGN KEY (event_id) REFERENCES events (id)
+    FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE CASCADE,
+    FOREIGN KEY (event_id) REFERENCES events (id) ON DELETE CASCADE
 );
+
 CREATE TABLE IF NOT EXISTS profiles (
     id SERIAL PRIMARY KEY,
     client_id INTEGER,
@@ -94,5 +104,5 @@ CREATE TABLE IF NOT EXISTS profiles (
     github_username TEXT,
     gitlab_username TEXT,
     x_username TEXT,
-    FOREIGN KEY (client_id) REFERENCES clients (id)
+    FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE CASCADE
 );
